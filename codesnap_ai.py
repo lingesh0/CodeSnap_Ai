e1fc3e27455684d7f564acee6d7d84f8e18e4e29
Lingesh <118330035+lingesh0@users.noreply.github.com>
2025-07-23 18:56:17 +0530

    Update codesnap_ai.py

--- codesnap_ai.py
+++ codesnap_ai.py
@@ -436,7 +436,7 @@ class GhostPrompt(QWidget):
                 load_dotenv()
                 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
                 if not GEMINI_API_KEY:
-                    GEMINI_API_KEY = "AIzaSyBIp7DNNfyAFxUQKiycBdRadMfvDZ06wmU"
+                    GEMINI_API_KEY = "Paste your gemini api key"
                 genai.configure(api_key=GEMINI_API_KEY)
                 model = genai.GenerativeModel('gemini-1.5-flash')
                 response = model.generate_content(code_prompt)
